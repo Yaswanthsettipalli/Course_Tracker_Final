26d949b4ab3b78b17ad84fe6d79a0e43e4ecd8f7
Yaswanthsettipalli <yaswanthsettipalli18@gmail.com>
2026-06-22 20:31:03 +0530

    r'

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -6,28 +6,7 @@ const db = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
-db.getConnection((err, connection) => {
-
-  if (err) {
-    console.error(
-      "Database connection failed:",
-      err
-    );
-    return;
-  }
-
-  console.log(
-    "MySQL Connected"
-  );
-
-  connection.release();
-
-});
-
-module.exports = db;
